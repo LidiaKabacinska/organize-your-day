dd9d4b52bbda7d85b9c9e90ca5db7d572fe79d12
LidiaKabacinska <kabacinska.lidia@gmail.com>
2018-07-15 20:35:25 +0200

    few visual changes plus some small corrections

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var editButton = document.querySelector("button");
-var Textinput = document.querySelector("input");
-var span = document.querySelectorAll("span");
-var input = document.querySelector("input[type='text']");
+let editButton = document.querySelector("button");
+let Textinput = document.querySelector("input");
+let span = document.querySelectorAll("span");
+let input = document.querySelector("input[type='text']");
 
 editButton.addEventListener("click", function() {
 Textinput.style.display="block";
@@ -9,16 +9,13 @@ Textinput.style.display="block";
 
 input.onkeypress = (function(evt) {
     if (evt.which == 13) {
-        var Todo = document.createElement("LI");
-        var TodoText = document.createTextNode(this.value);
-        TodoText.innerHTML = "<span>&times;</span> " + Todo;
-        Todo.appendChild(TodoText);
+        let Todo = document.createElement("LI"); 
+        let TodoText = this.value; 
+        Todo.innerHTML = '<span onclick="removeItem(this)">&#x2718;</span>' + TodoText; 
         document.getElementById("text-holder").appendChild(Todo);
     }
 })
 
-for (var i = 0; i < span.length; i++ ) {
-    span [i].addEventListener("click", function () {
-        this.parentElement.style.display="none";
-    });
+    function removeItem(elem) {
+        elem.parentElement.remove();
     }
